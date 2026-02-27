a786f3d1e31f6f040bf14aefcae2c695980daf01
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-27 19:28:59 +0200

    a button component recieving props from both stopwatch or timer

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+export default function ({ value, onClick, backgroundColor }) {
+  return (
+    <>
+      <button
+        onClick={onClick}
+        className="flex items-center justify-center font-bold bg-gray-500 p-2 rounded-md w-20 cursor-pointer"
+        style={{ backgroundColor: backgroundColor }}
+      >
+        {value}
+      </button>
+    </>
+  );
+}

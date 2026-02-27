500d447187b2b7d2fe3ad01ec9bef4b34298cab2
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-27 19:30:37 +0200

    imported the stopwatch component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/Button'
+import StopWatch from './components/StopWatch'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <StopWatch/>
     </>
   )
 }

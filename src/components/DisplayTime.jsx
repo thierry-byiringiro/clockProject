1c899ce9dcb79f7bfab580de40213a79beb5b105
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-28 17:54:05 +0200

    changing the color when the time is off

--- src/components/DisplayTime.jsx
+++ src/components/DisplayTime.jsx
@@ -1,13 +1,14 @@
 export default function DisplayTime({ elapsedTime }) {
+  const isFinished = elapsedTime === 0;
   let minutes = Math.floor(elapsedTime / 60000);
   let seconds = Math.floor((elapsedTime % 60000) / 1000);
-  let milliseconds = Math.floor((elapsedTime % 1000)/ 10)
+  let milliseconds = Math.floor((elapsedTime % 1000) / 10);
   minutes = String(minutes).padStart(2, "0");
   seconds = String(seconds).padStart(2, "0");
-  milliseconds = String(milliseconds).padStart(2,"0");
+  milliseconds = String(milliseconds).padStart(2, "0");
   return (
     <>
-      <div className="font-bold text-2xl">
+      <div className={`font-bold text-2xl ${isFinished ? "text-red-700" : ""}`}>
         {minutes}:{seconds}:{milliseconds}
       </div>
     </>

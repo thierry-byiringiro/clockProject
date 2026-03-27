c6c6d425fa36d067d41d509e48163108c93034f3
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-27 14:06:35 +0200

    added a div to center both the timer and stopwatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Button from './components/Button'
-import StopWatch from './components/StopWatch'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Button from "./components/Button";
+import StopWatch from "./components/StopWatch";
+import Timer from "./components/Timer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <StopWatch/>
+      <div className="flex  gap-5 items-center justify-center ">
+        <StopWatch />
+        <Timer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
